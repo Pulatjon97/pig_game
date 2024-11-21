df2bd1ad2f3d89b715ab8ebb2ee2bfafdad68a0c
Pulatjon <polatsultonov97@gmail.com>
2024-11-22 00:21:57 +0900

    refactor: added a comment

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
+// Starting conditions fdfdfdfdf
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
